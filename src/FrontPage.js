39775196e9a6c190e49dfe151667c69956a14131
Nathan Romriell <nate@rlabs.com>
2019-07-03 16:31:24 -0700

    First Commit - Features: Added Front Page Component, Added Front Page Cell Component TODO: Add Images, Create secondary pages

---
+++ src/FrontPage.js
@@ -0,0 +1,20 @@
+import FrontPageCell from "./FrontPageCell"
+import React from "react";
+
+var testData = [["Title 1", "https://storage.googleapis.com/gd-wagtail-prod-assets/original_images/evolving_google_identity_inline_002.gif", "https://www.google.com", 0],
+    ["Title 2", "https://storage.googleapis.com/gd-wagtail-prod-assets/original_images/evolving_google_identity_inline_002.gif", "https://www.google.com", 1],
+    ["Title 3", "https://storage.googleapis.com/gd-wagtail-prod-assets/original_images/evolving_google_identity_inline_002.gif", "https://www.google.com", 2],
+    ["Title 4", "https://storage.googleapis.com/gd-wagtail-prod-assets/original_images/evolving_google_identity_inline_002.gif", "https://www.google.com"], 3];
+
+
+const FrontPage = () => {
+
+    return (
+        <div>
+            {(testData).map(data =>
+            <FrontPageCell title={data[0]} image={data[1]} link={data[2]}/>)}
+        </div>
+    )
+};
+
+export default FrontPage;
